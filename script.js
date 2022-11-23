e37848ece901c029893a57882946e89a6315723a
Bruno Silva <115116287+Bruno-Alexandrini-Silva@users.noreply.github.com>
2022-11-23 18:42:14 -0300

    Inicia J.S

---
+++ script.js
@@ -0,0 +1,9 @@
+function fetchPokemons() {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+        .then((response) => response.json())
+        .then((response) => console.log(response))
+}
+
+fetchPokemons()
+
+
